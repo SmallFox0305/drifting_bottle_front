b6bd8175d05051fe6c9c464d222b1cd7cee177f4
Small_Fox <smallfox0305@gmail.com>
2026-06-05 09:04:30 +0800

    feat: 初始化项目

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup></script>
+
+<template>
+  <div class="drifting-bottle">
+    漂流瓶
+  </div>
+</template>
+
+<style scoped></style>
+
+<style>
+*,
+*::after,
+*::before {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+</style>
